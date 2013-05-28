c8f1ef9acff45ae7e1d86d85cccd671a94a18f1a
tobast <tobast@tobast.fr>
2013-05-28 23:08:22 +0200

    Has now configuration support / backup

---
+++ fr/tobast/bukkit/nomadicgameplay/ConfigManager.java
@@ -0,0 +1,124 @@
+
+/*
+ * PROGRAM:
+ *   NomadicGameplay - bukkit plugin
+ *
+ * AUTHOR:
+ *   Théophile BASTIAN (a.k.a. Tobast)
+ *
+ * CONTACT & WEBSITE:
+ *   http://tobast.fr/ (contact feature included)
+ *   error-report@tobast.fr (error reporting only)
+ *
+ * SHORT DESCRIPTION:
+ *   See first license line.
+ *
+ * LICENSE:
+ *   NomadicGameplay - Bukkit plugin. A new way to play minecraft - just as nomads
+ *   Copyright (C) 2013  Théophile BASTIAN
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see http://www.gnu.org/licenses/gpl.txt.
+*/
+
+package fr.tobast.bukkit.nomadicgameplay;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.Location;
+
+import fr.tobast.bukkit.nomadicgameplay.NomadicGameplay;
+
+public class ConfigManager {
+	private NomadicGameplay plugin;
+	private FileConfiguration conf;
+
+	public String mainWorld;
+	public int minTravelDistance;
+	public int daysBeforeInvasion;
+
+	ConfigManager(NomadicGameplay plugin) {
+		this.plugin = plugin;
+		conf = plugin.getConfig();
+		setDefaults();
+		loadConfig();
+	}
+
+	void setDefaults() {
+		// map.*
+		conf.addDefault("map.mainworld", "world");
+
+		// roam.*
+		conf.addDefault("roam.minTravelDistance", 500);
+		conf.addDefault("roam.daysBeforeInvasion", 3);
+
+		// gamestate.*
+		conf.addDefault("gamestate.lastPauseTime", 0);
+		conf.addDefault("gamestate.campLocation.x", -1);
+		conf.addDefault("gamestate.campLocation.y", -1);
+		conf.addDefault("gamestate.campLocation.z", -1);
+		conf.addDefault("gamestate.campLocation.world", "world");
+		conf.addDefault("gamestate.lastSetCampTime", 0);
+		conf.createSection("gamestate.playersids"); // Empty section @default
+		conf.addDefault("gamestate.mustTeleport", java.util.Collections.emptyList());
+
+		conf.options().copyDefaults(true);
+		plugin.saveConfig();
+	}
+
+	void loadConfig() {
+		mainWorld = conf.getString("map.mainworld");
+
+		minTravelDistance  = conf.getInt("roam.minTravelDistance");
+		daysBeforeInvasion = conf.getInt("roam.daysBeforeInvasion");
+
+		plugin.setLastPauseTime(conf.getLong("gamestate.lastPauseTime"));
+		Location campLoc = new Location(
+				plugin.getServer().getWorld(
+					conf.getString("gamestate.campLocation.world")),
+				conf.getInt("gamestate.campLocation.x"),
+				conf.getInt("gamestate.campLocation.y"),
+				conf.getInt("gamestate.campLocation.z"));
+		plugin.setCampInit(campLoc, conf.getInt("gamestate.lastSetCampTime"));
+
+		int nextPlayerId=0;
+		for(String player : conf.getConfigurationSection("gamestate.playersids").getKeys(false)) {
+			int id = conf.getInt("gamestate.playersids."+player);
+			plugin.setPlayerId(player, id);
+			if(id >= nextPlayerId)
+				nextPlayerId = id+1;
+		}
+		
+		plugin.setMustTeleport(conf.getBooleanList("gamestate.mustTeleport"));
+	}
+
+	void saveState() {
+		ConfigurationSection gsConf = conf.getConfigurationSection("gamestate");
+		
+		gsConf.set("lastPauseTime", plugin.getLastPauseTime());
+		gsConf.set("lastSetCampTime", plugin.getLastSetCampTime());
+		
+		Location campLoc = plugin.getCampLocation();
+		gsConf.set("campLocation.x", campLoc.getBlockX());
+		gsConf.set("campLocation.y", campLoc.getBlockY());
+		gsConf.set("campLocation.z", campLoc.getBlockZ());
+		gsConf.set("campLocation.world", campLoc.getWorld().getName());
+
+		for(String player : plugin.getPlayersNames()) {
+			gsConf.set("playersids."+player, plugin.getPlayerId(player));
+		}
+
+		gsConf.set("mustTeleport", plugin.getMustTeleport());
+	}
+}
+
--- fr/tobast/bukkit/nomadicgameplay/NomadicGameplay.java
+++ fr/tobast/bukkit/nomadicgameplay/NomadicGameplay.java
@@ -39,21 +39,28 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.World;
 
+import java.util.Set;
+import java.util.List;
 import java.util.HashMap;
 import java.util.ArrayList;
 import java.lang.IndexOutOfBoundsException;
 
 import fr.tobast.bukkit.nomadicgameplay.CommandHandler;
 import fr.tobast.bukkit.nomadicgameplay.EventListener;
+import fr.tobast.bukkit.nomadicgameplay.ConfigManager;
 
 public class NomadicGameplay extends JavaPlugin {
 	private CommandHandler cmdHandler = new CommandHandler(this);
-	private long lastPauseTime = 0; // TODO restore from saved state
+	private ConfigManager cfgManager = new ConfigManager(this);
+	private long lastPauseTime = 0; 
 	private HashMap<String, Integer> playersIds = new HashMap<String, Integer>();
 	private int nextPlayerId = 0;
-	private ArrayList<Boolean> mustTeleportPlayer = new ArrayList<Boolean>();
+	private ArrayList<Boolean> mustTeleportPlayer;
+	private World mainWorld;
 	private Location campLocation;
+	private long lastSetCampTime = 0;
 
 // ==== SETTERS/GETTERS ====
 	final long getLastPauseTime() {
@@ -62,7 +69,10 @@ public class NomadicGameplay extends JavaPlugin {
 	void setLastPauseTime(final long lastPauseTime) {
 		this.lastPauseTime = lastPauseTime;
 	}
-	
+
+	final Set<String> getPlayersNames() {
+		return playersIds.keySet();
+	}
 	final int getPlayerId(String name) {
 		Integer id = playersIds.get(name);
 
@@ -75,7 +85,16 @@ public class NomadicGameplay extends JavaPlugin {
 
 		return id;
 	}
+	void setPlayerId(String name, int id) {
+		playersIds.put(name,id);
+	}
+	void setNextPlayerId(int id) {
+		nextPlayerId = id;
+	}
 
+	final List<Boolean> getMustTeleport() {
+		return mustTeleportPlayer;
+	}
 	final boolean getMustTeleportPlayer(int playerId) {
 		boolean out;
 		try {
@@ -90,7 +109,13 @@ public class NomadicGameplay extends JavaPlugin {
 			return;
 		mustTeleportPlayer.set(playerId,val);
 	}
+	void setMustTeleport(List<Boolean> list) {
+		mustTeleportPlayer = new ArrayList<Boolean>(list);
+	}
 
+	final Location getCampLocation() {
+		return campLocation;
+	}
 	final Location getCampTeleportLocation() {
 		Location tpCampLoc = campLocation.clone();
 		while(!isValidTeleportLocation(tpCampLoc)) {
@@ -99,8 +124,28 @@ public class NomadicGameplay extends JavaPlugin {
 		return tpCampLoc;
 	}
 
+	final int distToCamp(Location loc) { // Manhattan shall be enough
+		return Math.abs(campLocation.getBlockX() - loc.getBlockX())
+			+ Math.abs(campLocation.getBlockZ() - loc.getBlockZ());
+	}
+	void setCamp(Location camp) {
+		// Here we suppose the tests have been made before.
+		// Indeed, an admin command shall be able to set camp everywhere.
+		campLocation = camp;
+		lastSetCampTime = mainWorld.getFullTime();
+	}
+	void setCampInit(Location camp, long lastSetCampTime) {
+		campLocation = camp;
+		this.lastSetCampTime = lastSetCampTime;
+	}
+
+	final long getLastSetCampTime() {
+		return lastSetCampTime;
+	}
+
 // ==== OVERLOADED BUKKIT API FUNCTIONS ====
 	public void onEnable() {
+		cfgManager.loadConfig();
 		initVars();
 		getServer().getPluginManager().registerEvents(new EventListener(this), this);
 	}
@@ -111,11 +156,15 @@ public class NomadicGameplay extends JavaPlugin {
 		return cmdHandler.onCommand(sender, command, label, args);
 	}
 
-//	public void onDisable {}
+	public void onDisable() {
+		cfgManager.saveState();
+	}
 
 // ==== MISC FUNCTIONS ====
 	private void initVars() {
-		campLocation = getServer().getWorld("world").getSpawnLocation(); // TODO config world
+		mainWorld = getServer().getWorld(cfgManager.mainWorld);
+		if(campLocation.getY() < 0) // Default value
+			campLocation = mainWorld.getSpawnLocation();
 	}
 
 	private boolean isValidTeleportLocation(final Location loc) {
